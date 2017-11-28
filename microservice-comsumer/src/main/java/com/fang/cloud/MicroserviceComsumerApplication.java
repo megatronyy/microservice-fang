83fbe1f8fedac414b099b1418b062cadaf3561c3
屈文斌 <twfx7758@hotmail.com>
2017-11-28 18:35:06 +0800

    fang

--- microservice-comsumer/src/main/java/com/fang/cloud/MicroserviceComsumerApplication.java
+++ microservice-comsumer/src/main/java/com/fang/cloud/MicroserviceComsumerApplication.java
@@ -2,8 +2,10 @@ package com.fang.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class MicroserviceComsumerApplication {
 
 	public static void main(String[] args) {
--- microservice-provider/src/main/java/com/fang/cloud/controller/LogInfoController.java
+++ microservice-provider/src/main/java/com/fang/cloud/controller/LogInfoController.java
@@ -3,6 +3,7 @@ package com.fang.cloud.controller;
 import com.fang.cloud.entity.LogInfo;
 import com.fang.cloud.repository.LogInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ public class LogInfoController {
     @Autowired
     private LogInfoRepository logInfoRepository;
 
+    @GetMapping("/log/{id}")
     public LogInfo findById(@PathVariable int id){
         LogInfo logInfo = this.logInfoRepository.findOne(id);
         return logInfo;
