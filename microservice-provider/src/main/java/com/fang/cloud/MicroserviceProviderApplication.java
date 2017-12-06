f726062ca6bc74150f1c9a1fe8bdd0f86c38b544
屈文斌 <twfx7758@hotmail.com>
2017-12-06 09:08:17 +0800

    接口完成

--- microservice-provider/src/main/java/com/fang/cloud/MicroserviceProviderApplication.java
+++ microservice-provider/src/main/java/com/fang/cloud/MicroserviceProviderApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.fang.cloud.mapper")
 public class MicroserviceProviderApplication {
 
